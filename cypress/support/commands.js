0e0d55df63a0134ae38debd77b65fbc954163a12
vaseoflcay <75212961+vaseoflcay@users.noreply.github.com>
2024-12-25 20:11:53 -0300

    Add files via upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,69 @@
-Cypress.Commands.add('token', (email, senha) => {
-    cy.request({
-        method: 'POST',
-        url: 'login',
-        body: {
-            "email": email,
-            "password": senha 
-        }
-    }).then((response) => {
-        expect(response.status).to.equal(200)
-        return response.body.authorization
-    })
- })
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
- Cypress.Commands.add('cadastrarProduto' , (token, produto, preco, descricao, quantidade) =>{
+//Cypress.Commands.add('token', (email, senha) => { 
+    //cy.request({
+       // method: 'POST',
+        //url: 'login',
+        //body:{
+          //"email": email,
+          //"password": senha
+        //}
+      //}).then(response =>{
+        //return response.body.authorization
+      //})
+
+//})
+
+//Cypress.Commands.add('cadastrarProduto', (token, produto, preco, descricao, quantidade) =>{
+  //  cy.request({
+    //    method:'POST',
+      //  url:'produtos',
+       // headers:{authorization: token},
+       // body:
+        //{
+         // "nome": produto,
+          //"preco": preco,
+          //"descricao": descricao,
+          //"quantidade": quantidade
+        //}, failOnStatusCode: false
+      //})
+//})
+
+Cypress.Commands.add('cadastrarUsuario', (usuario, email, senha, administrador) =>{
     cy.request({
-        method: 'POST', 
-        url: 'produtos',
-        headers: {authorization: token}, 
-        body: {
-            "nome": produto,
-            "preco": preco,
-            "descricao": descricao,
-            "quantidade": quantidade
-          }, 
-          failOnStatusCode: false
-    })
- })
+        method:'POST',
+        url:'usuarios',
+       // headers:{authorization: token},
+        body:
+        {
+            "nome": usuario,
+            "email": email,
+            "password": senha,
+            "administrador": administrador
+        }, failOnStatusCode: false
+      })
+})
